f2c7cc3f75ee1b48ed64e743868da47e0a8692c6
shade <382961818@qq.com>
2025-09-03 22:21:29 +0800

    modified:   src/11 copy.cpp

--- src/11 copy.cpp
+++ src/11 copy.cpp
@@ -28,68 +28,61 @@ private:
         
         int target = sequence[currentIndex];
         double result = 0.0;
-        
-        
-        if (!stack1.empty() && stack1.back() == target) {
-            stack1.pop_back();
-            result = solve(stack1, stack2, sequence, currentIndex + 1);
-            stack1.push_back(target);
-        }
-        
-        else if (!stack2.empty() && stack2.back() == target) {
-            stack2.pop_back();
-            result = solve(stack1, stack2, sequence, currentIndex + 1);
-            stack2.push_back(target);
-        }
-        
-        else {
-            std::vector<int> temp1 = stack1;
-            std::vector<int> temp2 = stack2;
-            double moves1 = 0.0, moves2 = 0.0;
-            bool found1 = false, found2 = false;
+if (!stack1.empty() && stack1.back() == target) {
+    stack1.pop_back();
+    result = 1.0 + solve(stack1, stack2, sequence, currentIndex + 1); 
+    stack1.push_back(target);
+}
+
+else if (!stack2.empty() && stack2.back() == target) {
+    stack2.pop_back();
+    result = 1.0 + solve(stack1, stack2, sequence, currentIndex + 1); 
+    stack2.push_back(target);
+}
+else {
+    std::vector<int> temp1 = stack1, temp2 = stack2;
+    double moves1 = 0.0, moves2 = 0.0;
+    bool found1 = false, found2 = false;
+
+    
+    for (int i = (int)temp1.size() - 1; i >= 0; --i) {
+        if (temp1[i] == target) {
+            found1 = true;
+            int movesCount = (int)temp1.size() - i - 1; 
             
-            
-            for (int i = temp1.size() - 1; i >= 0; i--) {
-                if (temp1[i] == target) {
-                    found1 = true;
-                    
-                    int movesCount = temp1.size() - i - 1;
-                    while (temp1.size() > i) {
-                        temp2.push_back(temp1.back());
-                        temp1.pop_back();
-                    }
-                    moves1 = movesCount + solve(temp1, temp2, sequence, currentIndex + 1);
-                    break;
-                }
+            while ((int)temp1.size() - 1 > i) {
+                temp2.push_back(temp1.back());
+                temp1.pop_back();
             }
             
-            temp1 = stack1;
-            temp2 = stack2;
-            
-            
-            for (int i = temp2.size() - 1; i >= 0; i--) {
-                if (temp2[i] == target) {
-                    found2 = true;
-                    
-                    int movesCount = temp2.size() - i - 1;
-                    while (temp2.size() > i) {
-                        temp1.push_back(temp2.back());
-                        temp2.pop_back();
-                    }
-                    moves2 = movesCount + solve(temp1, temp2, sequence, currentIndex + 1);
-                    break;
-                }
-            }
-            
-            if (found1 && found2) {
-                result = std::min(moves1, moves2);
-            } else if (found1) {
-                result = moves1;
-            } else if (found2) {
-                result = moves2;
-            }
+            temp1.pop_back();
+            moves1 = movesCount + 1.0 + solve(temp1, temp2, sequence, currentIndex + 1);
+            break;
         }
-        
+    }
+
+    temp1 = stack1; temp2 = stack2;
+
+    
+    for (int i = (int)temp2.size() - 1; i >= 0; --i) {
+        if (temp2[i] == target) {
+            found2 = true;
+            int movesCount = (int)temp2.size() - i - 1;
+            while ((int)temp2.size() - 1 > i) {
+                temp1.push_back(temp2.back());
+                temp2.pop_back();
+            }
+            temp2.pop_back(); 
+            moves2 = movesCount + 1.0 + solve(temp1, temp2, sequence, currentIndex + 1);
+            break;
+        }
+    }
+
+    
+    if (found1 && found2) result = std::min(moves1, moves2);
+    else if (found1)      result = moves1;
+    else if (found2)      result = moves2;
+}
         memo[state] = result;
         return result;
     }
